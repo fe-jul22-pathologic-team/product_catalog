8947b2961d0c3b15463ae4cc45cb8055f6561611
Jamal Amoian <leonidagutin09@gmail.com>
2022-10-31 13:50:27 +0200

    added HomePage folder

---
+++ catalog-product/src/Components/HomePage/Main/Main.tsx
@@ -0,0 +1,19 @@
+export const Main = () => {
+  return (
+    <>
+      <main className="main">
+        <section className="section">
+
+        </section>
+
+        <section className="section">
+          
+        </section>
+
+        <section className="section">
+          
+        </section>
+      </main>
+    </>
+  );
+};
