50ebbfb2b4a7d363ce6a948340d8e2b54c35441f
Jamal Amoian <leonidagutin09@gmail.com>
2022-10-31 15:27:03 +0200

    Added base markup from Main in HomePage

--- catalog-product/src/App.tsx
+++ catalog-product/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import { Main } from './Components/HomePage/Main';
 
 export function App() {
   return (
-    <h1>Hello Mate!</h1>
+    <>
+      <h1>Hello Mate!</h1>
+
+      <Main />
+    </>
   );
 }
