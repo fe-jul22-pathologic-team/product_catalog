05ba2183d6516d79b5a9ff62a307b7c9fd7c971c
Svyatoslav <svyatos1337@gmail.com>
2022-10-31 14:45:35 +0200

    added routers

--- catalog-product/src/App.tsx
+++ catalog-product/src/App.tsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 export function App() {
   return (
-    <h1>Hello Mate!</h1>
+    <>
+      <Routes>
+        <Route 
+          path="/catalog-product" 
+          element={<h1>Home page</h1>}
+        />
+
+        <Route 
+          path="/phones" 
+          element={<h1>Phone page</h1>}
+        />
+
+        <Route 
+          path="*" 
+          element={<p>Page not found</p>}
+        />
+      </Routes>
+    </>
   );
 }
