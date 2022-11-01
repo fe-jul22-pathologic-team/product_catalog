8e36cad57161607034866ecbaeb38312ae7f20e8
Nikita Havryliuk <chinter.games1@gmail.com>
2022-11-01 11:24:37 +0200

    added card

--- catalog-product/src/App.tsx
+++ catalog-product/src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css';
-import { CardItem } from './Components/HomePage/CardItem';
 import { HomePage } from './Components/HomePage/HomePage';
 
 export function App() {
   return (
     <HomePage />
-    <CardItem />
   );
 }
--- catalog-product/src/Components/HomePage/HomePage.tsx
+++ catalog-product/src/Components/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import { CardItem } from "./CardItem";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Main } from "./Main";
@@ -6,6 +7,7 @@ export const HomePage = () => {
   return (
     <>
       <Header />
+      <CardItem />
       <Main />
       <Footer />
     </>
