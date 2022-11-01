9943cf5648d26e8bb6a358c62d4f2681f42842c7
Nikita Havryliuk <chinter.games1@gmail.com>
2022-11-01 19:51:24 +0200

    some fixes

--- catalog-product/src/App.tsx
+++ catalog-product/src/App.tsx
@@ -10,7 +10,7 @@ export function App() {
           path="/" 
           element={<HomePage />}
         />
-        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
         <Route 
           path="/phones" 
           element={<Catalog />}
