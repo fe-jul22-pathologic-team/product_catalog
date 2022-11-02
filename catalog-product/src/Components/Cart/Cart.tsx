4b0a579f4ccfa34d94311fc75c49dddd8ae10d33
Svyatoslav <svyatos1337@gmail.com>
2022-11-02 15:44:15 +0200

    cart layout

--- catalog-product/src/Components/Cart/Cart.tsx
+++ catalog-product/src/Components/Cart/Cart.tsx
@@ -1,11 +1,51 @@
 import { Footer } from "../HomePage/Footer";
-import { Header } from "../HomePage/Header"
+import { Header } from "../HomePage/Header";
+import { Vector } from './Vector';
+import './Cart.css';
 
 export const Cart = () => {
   return (
     <>
     <Header />
-    <h1>Cart is coming</h1>
+      <div className="cart">
+        <div className="cart__container">
+          <div className="backButton">
+            <a href="back" className="backLink">
+              <Vector/>
+              Back
+            </a>
+          </div>
+          <span className="cart__logo">
+            Cart
+          </span>
+          <form 
+            className="cart__form" 
+            action="submit"
+            id="cart_form"
+          >
+            <ul className="cart__list">
+              <li className="cart__item">
+                cart items here
+              </li>
+            </ul>
+            <div className="cart__bill">
+              <div className="bill">
+                <span className="totalPrice">2000$</span>
+                <span className="totalItems">Total for 3 items</span>
+              </div>
+              <button 
+                className="bill__button" 
+                type="submit" 
+                form="cart__form" 
+                value="Submit"
+              >
+                Checkout
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      
     <Footer />
     </>
   );
