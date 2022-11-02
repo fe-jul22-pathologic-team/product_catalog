aa7a1a59b8779b3edaa33e0cc9caf06789a2c8c4
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-11-02 19:27:14 +0200

    display of pictures is implemented

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 'use strict';
 
+import * as url from 'url';
 import { getAll } from './services/products.js';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 export function createServer() {
   const express = require('express');
   const cors = require('cors');
   const app = express();
   app.use(cors());
-
   
   app.get('/products', express.json(), function (req, res) {
     res.statusCode = 200;
@@ -17,6 +18,8 @@ export function createServer() {
       res.send(response);
     });
   });
+
+  app.use(express.static(`${__dirname}/data`));
 
   return app;
 }
