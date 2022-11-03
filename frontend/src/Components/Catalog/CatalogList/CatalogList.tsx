eb5c98e3c4d08188f9cc593812a8b7adf10e368c
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-11-03 10:38:18 +0200

    renamed frontend

---
+++ frontend/src/Components/Catalog/CatalogList/CatalogList.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { getProducts } from "../../../api/products";
+import { Product } from "../../../types/Product";
+import { Loader } from "../../Loader";
+import { BASE_URL } from "../../../utils/fetchClient";
+
+type Props = {
+  phones: Product[];
+  from: number;
+  to: number;
+  handleAdd: (phone: Product) => void;
+  isLoading: boolean;
+};
+
+export const CatalogList: React.FC<Props> = ({
+  phones,
+  from,
+  to,
+  isLoading,
+  handleAdd,
+}) => {
+  return (
+    <>
+      {
+        isLoading
+          ? (
+            <div className="catalog__list">
+              {phones.slice(from - 1, to).map((phone) => (
+              <div className='phone' key={phone.id}>
+                <img
+                  src={`${BASE_URL}/${phone.image}`}
+                  alt={phone.name}
+                  className={'phone__image'}
+                />
+                <p className='phone__name'>
+                  {phone.name}
+                </p>
+
+                <div className='phone__price price'>
+                  <p className='price__current'>{`$${phone.price}`}</p>
+                  <p className="price__full">{`$${phone.fullPrice}`}</p>
+                </div>
+
+                <div className="characteristic">
+                  <p className="characteristic__title">Screen</p>
+                  <p className="characteristic__value">{phone.screen}</p>
+                </div>
+
+                <div className="characteristic">
+                  <p className="characteristic__title">Capacity</p>
+                  <p className="characteristic__value">{phone.capacity}</p>
+                </div>
+
+                <div className="characteristic characteristic--last">
+                  <p className="characteristic__title">RAM</p>
+                  <p className="characteristic__value">{phone.ram}</p>
+                </div>
+
+                <div className="buttons">
+                  <button className="buttons__buy" onClick={() => handleAdd(phone)}>Add to cart</button>
+                  <button className="buttons__favorite">
+                  </button>
+                </div>
+
+              </div>
+              ))}
+            </div>
+          ) : <Loader />
+      }
+    </>
+  );
+}
