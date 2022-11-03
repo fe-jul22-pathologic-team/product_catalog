8d1ffb7be554e6c9296ba2d81a29e2fe55da9f9e
Nikita Havryliuk <chinter.games1@gmail.com>
2022-11-03 17:15:18 +0200

    some fixes

---
+++ frontend/src/Components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import { BackToTop } from './BackToTop/BackToTop';
+import './Footer.scss';
+
+import { FooterNavigation } from "./Navigation/FooterNavigation";
+
+const logo = require("./logo.png");
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <img 
+        src={logo}
+        alt="Logo" 
+        className="footer__logo"
+      />
+      <FooterNavigation />
+      <BackToTop />
+    </footer>
+  );
+};
